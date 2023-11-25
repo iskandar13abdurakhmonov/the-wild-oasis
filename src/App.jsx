b5ee05b2a822f007f5b3cd1fc6ba90548f3195b3
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-11-26 00:51:13 +0500

    refactored Button and Input components into ui folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import styled from 'styled-components'
+import GlobalStyles from './styles/GlobalStyles'
+import Button from './ui/Button'
+import Input from './ui/Input'
 
 const H1 = styled.h1`
     font-size: 30px;
     font-weight: 600;
     background-color: yellow;
-`
-
-const Button = styled.button`
-    padding: 20px;
-    font-size: 1.4rem;
-    padding: 1.2rem 1.6rem;
-    font-weight: 500;
-    border: none;
-    border-radius: 7px;
-    background-color: purple;
-    color: white;
-    cursor: pointer;
-`
-
-const Input = styled.input`
-    border: 1px solid black;
-    border-radius: 5px;
-    padding: 0.8rem 1.2rem;
 `
 
 const StyledApp = styled.div`
@@ -31,16 +16,20 @@ const StyledApp = styled.div`
 
 function App() {
     return (
-        <StyledApp>
+        <>
+          <GlobalStyles/>
+          <StyledApp>
             <H1>The Wild Oasis</H1>
-            <Button onClick={() => alert('Check In')}>Check In</Button>
-            <Button onClick={() => alert('Check Out')}>Check out</Button>
+
+            <Button onClick={() => alert('Check in')}>Check in</Button>
+            <Button onClick={() => alert('Check out')}>Check out</Button>
 
             <Input
                 type="number"
                 placeholder="Number of guests"
             />
         </StyledApp>
+        </>
     )
 }
 
