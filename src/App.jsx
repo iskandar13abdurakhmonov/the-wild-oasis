0148e32dea8163766710218a9f964d8bea83a758
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-11-26 12:24:14 +0500

    passing props to styled components and making reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,48 @@ import styled from 'styled-components'
 import GlobalStyles from './styles/GlobalStyles'
 import Button from './ui/Button'
 import Input from './ui/Input'
-
-const H1 = styled.h1`
-    font-size: 30px;
-    font-weight: 600;
-    background-color: yellow;
-`
+import Heading from './ui/Heading'
+import Row from './ui/Row'
 
 const StyledApp = styled.div`
-    background-color: orangered;
     padding: 20px;
 `
 
 function App() {
     return (
         <>
-          <GlobalStyles/>
-          <StyledApp>
-            <H1>The Wild Oasis</H1>
+            <GlobalStyles />
+            <StyledApp>
+                <Row>
+                    <Row type="horizontal">
+                        <Heading as="h1">The Wild Oasis</Heading>
 
-            <Button onClick={() => alert('Check in')}>Check in</Button>
-            <Button onClick={() => alert('Check out')}>Check out</Button>
+                        <div>
+                            <Heading as="h2">Check in and out</Heading>
+                            <Button onClick={() => alert('Check in')}>
+                                Check in
+                            </Button>
+                            <Button variations='secondary' size='medium' onClick={() => alert('Check out')}>
+                                Check out
+                            </Button>
+                        </div>
+                    </Row>
 
-            <Input
-                type="number"
-                placeholder="Number of guests"
-            />
-        </StyledApp>
+                    <Row>
+                        <Heading as="h3">Forms</Heading>
+                        <form>
+                            <Input
+                                type="number"
+                                placeholder="Number of guests"
+                            />
+                            <Input
+                                type="number"
+                                placeholder="Number of guests"
+                            />
+                        </form>
+                    </Row>
+                </Row>
+            </StyledApp>
         </>
     )
 }
