9a2799945d9e1752531722ec69156e7b6fb7f40e
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-11-27 20:28:46 +0500

    added react toast and used it instead of ugly a** alerts

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -9,21 +9,30 @@ const StyledAppLayout = styled.div`
     grid-template-columns: 26rem 1fr;
     grid-template-rows: auto 1fr;
     height: 100vh;
-
 `
 
 const Main = styled.main`
     background-color: var(--color-grey-50);
     padding: 4rem 4.8rem 6.4rem;
-`;
+`
+
+const Container = styled.div`
+    max-width: 120rem;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    gap: 3.2rem;
+`
 
 export default function AppLayout() {
     return (
         <StyledAppLayout>
-            <Header/>
-            <Sidebar/>
+            <Header />
+            <Sidebar />
             <Main>
-                <Outlet/>
+                <Container>
+                    <Outlet />
+                </Container>
             </Main>
         </StyledAppLayout>
     )
