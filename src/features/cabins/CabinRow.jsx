dcc01fd5aa2be6129cb53d1f8a7072f3aac51582
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-12-03 23:41:49 +0500

    buidling a reusable table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -8,18 +8,19 @@ import { HiSquare2Stack, HiPencil, HiTrash } from 'react-icons/hi2'
 import { useCreateCabin } from './useCreateCabin'
 import Modal from '../../ui/Modal'
 import ConfirmDelete from '../../ui/ConfirmDelete'
+import Table from '../../ui/Table'
 
-const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-    padding: 1.4rem 2.4rem;
+// const TableRow = styled.div`
+//     display: grid;
+//     grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//     column-gap: 2.4rem;
+//     align-items: center;
+//     padding: 1.4rem 2.4rem;
 
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-`
+//     &:not(:last-child) {
+//         border-bottom: 1px solid var(--color-grey-100);
+//     }
+// `
 
 const Img = styled.img`
     display: block;
@@ -74,7 +75,7 @@ export default function CabinRow({ cabin }) {
     }
 
     return (
-        <TableRow role="row">
+        <Table.Row>
             <Img src={image} />
             <Cabin>{name}</Cabin>
             <div>Fits up to {maxCapacity}</div>
@@ -102,7 +103,7 @@ export default function CabinRow({ cabin }) {
                         <CreateCabinForm cabinToEdit={cabin} />
                     </Modal.Window>
 
-                    <Modal.Open>
+                    <Modal.Open opens="delete">
                         <button
                             onClick={() => deleteCabin(cabinId)}
                             disabled={isDeleting}
@@ -110,7 +111,7 @@ export default function CabinRow({ cabin }) {
                             <HiTrash />
                         </button>
                     </Modal.Open>
-                    <Modal.Window>
+                    <Modal.Window name="delete">
                         <ConfirmDelete
                             resourceName="cabins"
                             disabled={isDeleting}
@@ -119,6 +120,6 @@ export default function CabinRow({ cabin }) {
                     </Modal.Window>
                 </Modal>
             </div>
-        </TableRow>
+        </Table.Row>
     )
 }
