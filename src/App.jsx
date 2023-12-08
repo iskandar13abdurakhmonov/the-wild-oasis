185bd9d8c29795fbe2ec67d9d044798614d4a7b0
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-12-08 17:29:41 +0500

    building the single booking page

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Login from './pages/Login'
 import PageNotFound from './pages/PageNotFound'
 import AppLayout from './ui/AppLayout'
 import { Toaster } from 'react-hot-toast'
+import Booking from './pages/Booking'
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -48,6 +49,10 @@ export default function App() {
                         <Route
                             path="bookings"
                             element={<Bookings />}
+                        />
+                        <Route
+                            path="bookings/:bookingId"
+                            element={<Booking />}
                         />
                         <Route
                             path="cabins"
