2b332404c889ac6d9b00ab030f44112de3f892c1
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-12-15 20:43:55 +0500

    user sign up

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,15 +3,23 @@ import Button from '../../ui/Button'
 import Form from '../../ui/Form'
 import FormRow from '../../ui/FormRow'
 import Input from '../../ui/Input'
+import { useSignup } from './useSignup'
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-    const { register, formState, getValues, handleSubmit } = useForm()
+    const { signup, isLoading } = useSignup()
+
+    const { register, formState, getValues, handleSubmit, reset } = useForm()
     const { errors } = formState
 
-    function onSubmit(data) {
-      console.log(data)
+    function onSubmit({ fullName, email, password }) {
+        signup(
+            { fullName, email, password },
+            {
+                onSettled: () => reset(),
+            }
+        )
     }
 
     return (
@@ -23,6 +31,7 @@ function SignupForm() {
                 <Input
                     type="text"
                     id="fullName"
+                    disabled={isLoading}
                     {...register('fullName', {
                         required: 'This field is required!',
                     })}
@@ -36,6 +45,7 @@ function SignupForm() {
                 <Input
                     type="email"
                     id="email"
+                    disabled={isLoading}
                     {...register('email', {
                         required: 'This field is required!',
                         pattern: /\S+@\S+\.\S+/,
@@ -51,6 +61,7 @@ function SignupForm() {
                 <Input
                     type="password"
                     id="password"
+                    disabled={isLoading}
                     {...register('password', {
                         required: 'This field is required!',
                         minLength: {
@@ -68,6 +79,7 @@ function SignupForm() {
                 <Input
                     type="password"
                     id="passwordConfirm"
+                    disabled={isLoading}
                     {...register('passwordConfirm', {
                         required: 'This field is required!',
                         validate: (value) =>
@@ -82,10 +94,11 @@ function SignupForm() {
                 <Button
                     variation="secondary"
                     type="reset"
+                    disabled={isLoading}
                 >
                     Cancel
                 </Button>
-                <Button>Create new user</Button>
+                <Button disabled={isLoading}>Create new user</Button>
             </FormRow>
         </Form>
     )
