9109d1a5b67f2970233f8d770fe758c0ed8b13bf
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-12-16 20:16:24 +0500

    displaying charts

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,30 +1,37 @@
-import styled from "styled-components";
-import { useRecentBookings } from "./useRecentBookings";
+import styled from 'styled-components'
+import { useRecentBookings } from './useRecentBookings'
 import { useRecentStays } from './useRecentStays'
+import { useCabins } from '../cabins/useCabins'
 import Spinner from '../../ui/Spinner'
+import Stats from './Stats'
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto 34rem auto;
+    gap: 2.4rem;
 `
 
 export default function DashboardLayout() {
+    const { bookings, isLoading: isLoading1 } = useRecentBookings()
+    const { stays, confirmedStays, isLoading: isLoading2, numDays } = useRecentStays()
+    const {cabins, isLoading: isLoading3 } = useCabins()
 
-  const { bookings, isLoading: isLoading1 } = useRecentBookings()
-  const { stays, confirmedStays, isLoading: isLoading2} = useRecentStays()
+    console.log(bookings)
 
-  console.log(bookings)
+    if (isLoading1 || isLoading2 || isLoading3) return <Spinner />
 
-  if(isLoading1 || isLoading2) return <Spinner/>
-
-  return (
-    <StyledDashboardLayout>
-      <div>Statistics</div>
-      <div>Todays activities</div>
-      <div>Chart stay durations</div>
-      <div>Chart sales</div>
-    </StyledDashboardLayout>
-  )
+    return (
+        <StyledDashboardLayout>
+            <Stats
+                bookings={bookings}
+                confirmedStays={confirmedStays}
+                numDays={numDays}
+                cabinCount={cabins.length}
+            />
+            <div>Todays activities</div>
+            <div>Chart stay durations</div>
+            <div>Chart sales</div>
+        </StyledDashboardLayout>
+    )
 }
