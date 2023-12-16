c5fc169bcd0bf7f16060bfae5046294ac66951d1
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-12-16 17:22:48 +0500

    implemented dark mode with css variables

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -36,8 +36,6 @@ export async function getCurrentUser() {
 
     const { data, error } = await supabase.auth.getUser()
 
-    console.log(data)
-
     if (error) throw new Error(error.message)
 
     return data?.user
@@ -66,11 +64,13 @@ export async function updateCurrentUser({ password, fullName, avatar }) {
 
     if (storageError) throw new Error(storageError.message)
 
-    const { data: updatedUser, error: error2 } = supabase.auth.updateUser({
-        data: {
-            avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
-        },
-    })
+    const { data: updatedUser, error: error2 } = await supabase.auth.updateUser(
+        {
+            data: {
+                avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+            },
+        }
+    )
 
     if (error2) throw new Error(error2.message)
     return updatedUser
