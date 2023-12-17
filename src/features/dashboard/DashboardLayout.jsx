3b96797148e04b3f6cf274e35678f598c8ec92de
Iskandar Abdurakhmonov <iskandar.13.aburahmonov@gmail.com>
2023-12-17 22:25:51 +0500

    displaying stay for current day

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Spinner from '../../ui/Spinner'
 import Stats from './Stats'
 import SalesChart from './SalesChart'
 import DurationChart from './DurationChart'
+import TodayActivity from '../check-in-out/TodayActivity'
 
 const StyledDashboardLayout = styled.div`
     display: grid;
@@ -34,7 +35,7 @@ export default function DashboardLayout() {
                 numDays={numDays}
                 cabinCount={cabins.length}
             />
-            <div>Todays activities</div>
+            <TodayActivity/>
             <DurationChart confirmedStays={confirmedStays} />
             <SalesChart
                 bookings={bookings}
